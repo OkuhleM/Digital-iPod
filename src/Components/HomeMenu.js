05a76ba86870b09c24499f797a3cd3c9c9451b38
OkuhleM <lindokuhlea38@gmail.com>
2026-02-07 23:00:33 +0200

    add components

--- src/Components/HomeMenu.js
+++ src/Components/HomeMenu.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function HomeMenu() {
   return (
-    <div>HomeMenu</div>
+    <div>
+        <div className='Container'>
+            <div className='card'>
+                <div className='round-btn'></div>
+            </div>
+        </div>
+    </div>
   )
 }
 
