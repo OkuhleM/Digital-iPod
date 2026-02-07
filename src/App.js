e2c6d1f845f19b64fd9e2d43acc4819fde602ed0
OkuhleM <lindokuhlea38@gmail.com>
2026-02-07 22:55:34 +0200

    add components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Display from "./Components/Display";
+
+
+function App() {
+  return (
+    <div className="App">
+      < Display />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Display.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeMenu from './HomeMenu'
+
+function Display() {
+  return (
+    <div>
+        <HomeMenu />
+    </div>
+  )
+}
+
+export default Display
